4324df5ea88c72b2414f0167638734fd7ac502ee
luiscdo <luiscdo1997@gmail.com>
2018-12-27 16:18:02 -0400

    img login

--- wep_api2/src/components/Login/Login.js
+++ wep_api2/src/components/Login/Login.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
 import firebase from 'firebase'
+import '../Global/css/Login.css'
 
+// Utils
+import auth from '../../utils/auth';
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: null
-        }
+            user: null,
+            pictures:[],
+            estado:'galeria',
+            email:"",
+            password:""
+        };
         this.handleAuthGoogle = this.handleAuthGoogle.bind(this);
         this.handleLogout = this.handleLogout.bind(this);
+        this.handleUpload = this.handleUpload.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSumit = this.handleSumit.bind(this);
+        this.Auth = new auth('http://localhost:8080/users');
+
     }
 
     componentWillMount(){
         firebase.auth().onAuthStateChanged(user => {this.setState({user})});
+
+        firebase.database().ref('pictures').on('child_added',snapshot =>{
+            this.setState({
+                pictures: this.state.pictures.concat(snapshot)
+            });
+        })
     }
 
     handleAuthGoogle(){
@@ -24,51 +42,102 @@ class Login extends Component {
             .catch(error => console.log(`error ${error.code} : ${error.message}`))
     }
 
+    handleUpload(event){
+        const files = event.target.files;
+        const file = files[0];
+        const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
+        const task = storageRef.put(file);
+
+        task.on('state_changed', snapshot =>{
+                let porcentaje = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                this.setState({
+                    uploadValue : porcentaje
+                });
+            }, error => {
+                console.log(error.message)
+            }, () =>{
+                task.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                    const record = {
+                      photoURL: this.state.user.photoURL,
+                      displayName: this.state.user.displayName,
+                      email: this.state.user.email,
+                      image: downloadURL
+                    };
+
+                    console.log(record);
+
+                    const dbRef = firebase.database().ref('pictures');
+                    const newPicture = dbRef.push();
+                    newPicture.set(record);
+                });
+            }
+        )
+    }
 
     handleLogout(){
         firebase.auth().signOut()
             .then(result => console.log(`${result.user.email} ha cerrado sesión`))
             .catch(error => console.log(`error ${error.code} : ${error.message}`))
     }
-    /*
-    handleAuthFacebook(){
-        const providerGoogle = new firebase.auth.FacebookAuthProvider();
 
-        firebase.auth().signInWithPopup(providerGoogle)
-            .then(result => {
-                console.log(`${result.user.getIdToken()} ha iniciado sesión`)
-                this.setState({logeado:true});
-            })
-            .catch(error => console.log(`error ${error.code} : ${error.message}`))
-    }*/
+    handleDate(){
+        if(this.state.estado !== 'galeria'){
+            this.setState({estado:'galeria'});
+        }
+    }
 
     renderLogin(){
         if(this.state.user){
             return(
-              <div>
-                  <img src={this.state.user.photoURL} alt={this.state.user.displayName}/>
-                  <p> Bienvenido {this.state.user.displayName}</p>
-                  <button onClick={this.handleLogout}>Salir</button>
-              </div>
+                /*<Perfil/>*/
+                <button type="button" className="btn btn-success btn-sm" onClick={this.handleLogout}>Salir</button>
             );
         }else{
             return(
                 <div>
-                    <h4>ingrese al sistema</h4>
-                    <button className="pt-button pt-intent-primary" onClick={() => {this.handleAuthGoogle()}}>
-                        Login con Google
-                    </button>
+                    <div className="card card-container">
+                        <img id="profile-img" className="profile-img-card" src="../Global/images/login.png" alt=""/>
+                        <p id="profile-name" className="profile-name-card"/>
+                        <form className="form-signin">
+                            <span id="reauth-email" className="reauth-email"/>
+                            <input defaultValue={this.state.email} type="email" id="inputEmail" name="email" className="form-control" placeholder="correo" onChange={this.handleChange} required />
+                                <input defaultValue={this.state.password} type="password" id="inputPassword"  name="password" className="form-control" placeholder="contraseña" onChange={this.handleChange} required/>
+                                    <button className="btn btn-lg btn-primary btn-block btn-signin" type="submit" onClick={this.handleSumit}>Ingresar</button>
+                                    <button className="pt-button pt-intent-primary" onClick={() => {this.handleAuthGoogle()}}>
+                                        Ingresar con Google
+                                    </button>
+                        </form>
+                        <a href={this.handleDate()} className="forgot-password">
+                            olvido su contraseña?
+                        </a>
+                    </div>
                 </div>
             );
         }
     }
 
+    handleChange(e){
+        const {value, name} = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSumit(e){
+        e.preventDefault();
+        this.Auth.login(this.state.email,this.state.password)
+            .then(resp =>{
+                this.props.history.replace('/');
+            })
+            .catch(err =>{
+                alert(err)
+            })
+    }
 
     render() {
         return (
             <div className="Login">
                 {this.renderLogin()}
-
             </div>
         );
     }
